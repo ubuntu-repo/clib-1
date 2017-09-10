6b8eae312f6d331d93bb492a26c0255e6f893855
Joseph Cameron <jf.cameron@gmail.com>
2017-09-10 15:16:34 -0400

    cleanign

--- workspace/osx/Clib.build/Debug/ALL_BUILD.build/Script-58C620BA764D422F9B1BDADE.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-make -C /Users/joseph.cameron/Workspace/clib/workspace/osx -f /Users/joseph.cameron/Workspace/clib/workspace/osx/CMakeScripts/ALL_BUILD_cmakeRulesBuildPhase.make$CONFIGURATION all
--- workspace/osx/Clib.build/Debug/ZERO_CHECK.build/Script-41FD2503F3E9463997C7F97A.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-make -C /Users/joseph.cameron/Workspace/clib/workspace/osx -f /Users/joseph.cameron/Workspace/clib/workspace/osx/CMakeScripts/ZERO_CHECK_cmakeRulesBuildPhase.make$CONFIGURATION all
